9cce947567a00dabf2ef2d59fe4980d4fcd05bf7
Buse Şahinbaş <buse.sahinbas@loodos.com>
2024-11-16 18:40:01 +0300

    Create project

---
+++ MeditationApp/MeditationAppTests/MeditationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationAppTests.swift
+//  MeditationAppTests
+//
+//  Created by Buse Şahinbaş on 16.11.2024.
+//
+
+import Testing
+@testable import MeditationApp
+
+struct MeditationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
