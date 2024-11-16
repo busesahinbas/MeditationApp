f31a859084fa8da0143d5901742e3d8ea42acf6e
Edanur-Yildirim <Edanur.Yildirim@vodafone.com>
2024-11-17 00:57:09 +0300

    Create Information Page

---
+++ MeditationApp/MeditationApp/Information/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  MeditationApp
+//
+//  Created by Edanur Yildirim, Vodafone on 17.11.2024.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    @IBOutlet weak var infoTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
