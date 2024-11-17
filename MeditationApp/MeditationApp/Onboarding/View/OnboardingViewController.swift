c20df7c7f73f2e1811722e23df43c5350bdd3276
Buse Şahinbaş <buse.sahinbas98@gmail.com>
2024-11-17 15:12:04 +0300

    Create onboarding methods

---
+++ MeditationApp/MeditationApp/Onboarding/View/OnboardingViewController.swift
@@ -0,0 +1,75 @@
+//
+//  OnboardingViewController.swift
+//  MeditationApp
+//
+//  Created by Buse Şahinbaş on 17.11.2024.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    @IBOutlet private(set) weak var titleLabel: UILabel!
+    @IBOutlet private(set) weak var descriptionLabel: UILabel!
+    @IBOutlet private(set) weak var nextButton: UIButton!
+    @IBOutlet private(set) weak var pageControl: UIPageControl!
+
+    private var pages: [OnboardingPage] = []
+    private var currentPage = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        pages = [
+            OnboardingPage(title: "Welcome", description: "Discover new ways to meditate."),
+            OnboardingPage(title: "Relax", description: "Find peace and balance in life."),
+            OnboardingPage(title: "Focus", description: "Stay sharp and productive.")
+        ]
+
+        setup()
+    }
+
+    func setup() {
+        setupPageControl()
+        nextButton.setTitleColor(.white, for: .normal)
+        nextButton.layer.cornerRadius = 10
+
+    }
+
+    func setupPageControl() {
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = currentPage
+        updatePage()
+    }
+
+    func configure(with title: String, description: String) {
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        if currentPage < pages.count - 1 {
+            currentPage += 1
+            updatePage()
+        } else {
+            navigateToMainApp()
+        }
+    }
+
+    private func navigateToMainApp() {
+      //TODO: navigate to dashboard
+    }
+    
+    private func updatePage() {
+        let page = pages[currentPage]
+        titleLabel.text = page.title
+        descriptionLabel.text = page.description
+
+        if currentPage == pages.count - 1 {
+            nextButton.setTitle("Get Started", for: .normal)
+        } else {
+            nextButton.setTitle("Next", for: .normal)
+        }
+
+        pageControl.currentPage = currentPage
+    }
+}
