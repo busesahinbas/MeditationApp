3c7af57a19273b22d8944726ee480886adb11c8b
Buse Şahinbaş <buse.sahinbas98@gmail.com>
2024-11-17 15:11:09 +0300

    Create onboarding model

---
+++ MeditationApp/MeditationApp/Onboarding/Model/OnboardingModel.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingModel.swift
+//  MeditationApp
+//
+//  Created by Buse Şahinbaş on 17.11.2024.
+//
+
+import Foundation
+
+struct OnboardingPage {
+    let title: String
+    let description: String
+}
