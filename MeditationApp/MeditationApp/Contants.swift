5bc137f74f7b4e6132a36613bb246a7b38163aff
busesahinbas <buse.sahinbas98@gmail.com>
2024-12-02 17:15:16 +0300

    Create Constants

---
+++ MeditationApp/MeditationApp/Contants.swift
@@ -0,0 +1,18 @@
+//
+//  Contants.swift
+//  MeditationApp
+//
+//  Created by Buse Şahinbaş on 2.12.2024.
+//
+
+import Foundation
+
+enum Constants {
+    enum ImageNames {
+        enum Onboarding {
+            static let firstImage = "onboarding1"
+            static let secondImage = "onboarding2"
+            static let thirdImage = "onboarding3"
+        }
+    }
+}
