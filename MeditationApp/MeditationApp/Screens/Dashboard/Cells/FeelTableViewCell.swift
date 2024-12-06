8a0cb1666c655e53750b9ade814049b05dd671ad
busesahinbas <buse.sahinbas98@gmail.com>
2024-12-06 18:46:48 +0300

    Create FeelTableViewCell

---
+++ MeditationApp/MeditationApp/Screens/Dashboard/Cells/FeelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeelTableViewCell.swift
+//  MeditationApp
+//
+//  Created by Buse Şahinbaş on 6.12.2024.
+//
+
+import UIKit
+
+class FeelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
