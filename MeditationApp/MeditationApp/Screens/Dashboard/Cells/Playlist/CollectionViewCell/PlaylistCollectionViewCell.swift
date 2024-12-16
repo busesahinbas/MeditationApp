ba18ec729461e59896b8fda842fa29956dd2a04c
busesahinbas <buse.sahinbas98@gmail.com>
2024-12-16 23:43:25 +0300

    Feat add corner radius on PlaylistCollectionViewCell

--- MeditationApp/MeditationApp/Screens/Dashboard/Cells/Playlist/CollectionViewCell/PlaylistCollectionViewCell.swift
+++ MeditationApp/MeditationApp/Screens/Dashboard/Cells/Playlist/CollectionViewCell/PlaylistCollectionViewCell.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 
-class PlaylistCollectionViewCell: UICollectionViewCell {
-
+final class PlaylistCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private(set) weak var mainView: UIView!
+    @IBOutlet private(set) weak var imageView: UIImageView!
+    @IBOutlet private(set) weak var titleLabel: UILabel!
+    @IBOutlet private(set) weak var minView: UIView!
+    @IBOutlet private(set) weak var minLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setup()
     }
 
+    func setup() {
+        imageView.layer.cornerRadius = 10
+        mainView.layer.cornerRadius = 10
+        minView.layer.cornerRadius = 5
+    }
 }
