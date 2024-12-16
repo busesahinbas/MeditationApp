88b6aa0add9f2ec88c24c60c7078098c44630954
busesahinbas <buse.sahinbas98@gmail.com>
2024-12-16 22:42:23 +0300

    Create PlaylistCollectionViewCell

---
+++ MeditationApp/MeditationApp/Screens/Dashboard/Cells/Playlist/CollectionViewCell/PlaylistCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlaylistCollectionViewCell.swift
+//  MeditationApp
+//
+//  Created by Buse Şahinbaş on 16.12.2024.
+//
+
+import UIKit
+
+class PlaylistCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
